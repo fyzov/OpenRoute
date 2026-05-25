a1f74409a91ff18eaf376a5c285ce3a5f2c604ef
fyzov <fyzovfyzov@gmail.com>
2026-05-25 20:16:53 +0500

    feat: add basic logic

--- source/main.py
+++ source/main.py
@@ -1,6 +1,14 @@
-def main():
-    pass
+from utils import parse_args
+from services import search
 
+def main():
+    args = parse_args()
+
+    match args.command:
+        case "search":
+            search.execute(args.prompt)
+        case _:
+            print(f"Unknown command {args.command}")
 
 if __name__ == "__main__":
     main()
