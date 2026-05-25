93c5985d7c68bf95f5f2ded641ba7a229456cc8e
fyzov <fyzovfyzov@gmail.com>
2026-05-25 20:15:45 +0500

    feat: add search functional

---
+++ source/services/search.py
@@ -0,0 +1,6 @@
+from utils import build_url, open_in_browser
+
+def execute(prompt: str) -> None:
+    base_url = "https://duckduckgo.com/?q="
+    url = build_url(base_url, prompt)
+    open_in_browser(url)
