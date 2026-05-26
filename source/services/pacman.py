f435af971c8ed11a6b680b6da84547e9d455b601
fyzov <fyzovfyzov@gmail.com>
2026-05-26 12:02:18 +0500

    feat(service): add pacman repository support

--- source/services/pacman.py
+++ source/services/pacman.py
@@ -1,10 +1,9 @@
 from utils import build_url, open_in_browser
-
 from services import service
 
 
-@service("dockerhub")
+@service("pacman")
 def execute(prompt: str) -> None:
-    base_url = "https://hub.docker.com/search?q="
+    base_url = "https://archlinux.org/packages/?q="
     url = build_url(base_url, prompt)
     open_in_browser(url)
