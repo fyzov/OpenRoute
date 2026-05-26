fa65c14684737ab27ab04904d005480c1fd44b85
fyzov <fyzovfyzov@gmail.com>
2026-05-26 11:51:54 +0500

    feat(service): add dockerhub support

---
+++ source/services/dockerhub.py
@@ -0,0 +1,8 @@
+from utils import build_url, open_in_browser
+from services import service
+
+@service("dockerhub")
+def execute(prompt: str) -> None:
+    base_url = "https://hub.docker.com/search?q="
+    url = build_url(base_url, prompt)
+    open_in_browser(url)
